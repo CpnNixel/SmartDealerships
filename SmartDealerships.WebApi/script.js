00cb3af2a480daec5dd1ff981371f0780241ce49
Mykyta Kysil <mykyta.kysil@formpipe.com>
2022-06-19 23:21:10 +0300

    Added k6 load test

---
+++ SmartDealerships.WebApi/script.js
@@ -0,0 +1,25 @@
+﻿import { check } from 'k6';
+import http from 'k6/http';
+
+
+export const options = {
+    vus: 10,
+    duration: '1m',
+    insecureSkipTLSVerify: true
+}
+
+export default function () {
+   const url = 'http://localhost:5088/get-all-products';
+
+    const params = {
+        headers: {
+            'Content-Length' : 0,
+        }
+    }
+
+    const res = http.get(url, params);
+
+    check(res, {
+        'is status 400': (r) => r.status = 400,
+    });
+}
